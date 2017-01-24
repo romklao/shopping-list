7c9c803731130f81ac6817a1bccf16139bb3dc4f
romklao <rk120879@gmail.com>
2017-01-23 16:35:10 -0800

    Fixed delete button

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ var state = {
     items: []
 };
 
-var renderList = function(state, element) {
+var renderList = function(state, element, itemDataAttr) {
     var itemsHTML = state.items.map(function(item, index) {
-      return '<li>' +
-                '<span class="shopping-item js-shopping-item">' + item + '</span>' +
+      return '<li data-list-item-id=' + index + '>' +
+                '<span class="shopping-item js-shopping-item' + (item.checked ? ' shopping-item__checked' : '') + '">' + item.item + '</span>' +
                 '<div class="shopping-item-controls">' +
                       '<button class="js-shopping-item-toggle">' +
                         '<span class="button-label">check</span>' +
@@ -20,11 +20,22 @@ var renderList = function(state, element) {
 }
 
 function addItem(state, item) {
-    return state.items.push(item);        
+    return state.items.push({
+      item: item,
+      checked: false
+    });        
 };
 
-function getItem(state, itemIndex) {
-    return state.items[itemIndex];
+function getItem(state, index) {
+    return state.items[index];
+}
+
+function updateItem(state, index, newState) {
+    state.items[index] = newState;
+}
+
+function deleteItem(state, index) {
+    state.items.splice(index, 1);
 }
 
 function setupFormSubmit() {
@@ -36,26 +47,32 @@ function setupFormSubmit() {
 }
 
 function setupItemToggle() {
-    $('.js-shopping-list').on('click', $('.js-shopping-item-toggle'), function(event) {
-        console.log('hi', $(this).closest('li span'));
-
-        $(this).find('.js-shopping-item').toggleClass('shopping-item__checked')
+    $('.js-shopping-list').on('click', '.js-shopping-item-toggle', function(event) {
+        //console.log('hi', $(this).parent().parent().attr('data-list-item-id'));
+        var index = $(this).parent().parent().attr('data-list-item-id');
+        updateItem(state, index, {
+            item: state.items[index].item,
+            checked: !state.items[index].checked
+        });
+        renderList(state, $('.js-shopping-list'));
     });
 }
 
-function deleteItem() {
-    $('.js-shopping-item-delete').click(function(event) {
-        $('ul').remove();
-    })
+function setupItemDelete() {
+    $('.js-shopping-list').on('click', '.js-shopping-item-delete', function(event) {
+        var index = $(this).parent().parent().attr('data-list-item-id');
+        deleteItem(state, index);
+        renderList(state, $('.js-shopping-list'));
+
+    });
 }
 
 $(function() {
+  var itemDataAttr = 'data-list-item-id';
   setupFormSubmit();
   setupItemToggle();
-  deleteItem();
-});
-
-
+  setupItemDelete();
+  });
 
 
 
